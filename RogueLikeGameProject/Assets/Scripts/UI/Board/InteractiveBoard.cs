26d6fd35b8bb098e0536e77867f892ab81c8dc10
anyue <1183767577@qq.com>
2022-03-30 18:00:22 +0800

    临时提交

---
+++ RogueLikeGameProject/Assets/Scripts/UI/Board/InteractiveBoard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UFramework.GameCommon;
+using UnityEngine;
+
+public class InteractiveBoard : BaseUI
+{
+    // [SerializeField] private rect
+    public override void OnShow(params object[] args)
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    #region 移动端移动逻辑
+
+    private void MobileInput()
+    {
+        if (Input.touchCount <= 0)
+        {
+            return;
+        }
+
+        Touch touch = Input.touches[0];
+        // RectTransformUtility.ScreenPointToLocalPointInRectangle()
+    }
+
+    private void TouchStart(Touch touch)
+    {
+    }
+
+    private void TouchMove(Touch touch)
+    {
+    }
+
+    private void TouchEnd(Touch touch)
+    {
+    }
+
+    #endregion
+}
